92e3dd3198355fc666f4ae05fb2300aefafbebe5
shravya-chepa <riya.chepa@gmail.com>
2023-06-07 14:59:15 +0530

    fix: nav-bar position feat: add home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.scss";
 
 import NavigationBar from "./components/NavigationBar";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
+      <br />
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import "./Home.scss";
+import profile from "../assets/profile_photo.png";
+
+function Home() {
+  return (
+    <div className="home-container">
+      <div className="home_intro">
+        <h1>Hello, </h1>
+        <br />
+        <h1>I am</h1>
+        <h1 class="home_intro-name">Shravya</h1>
+        <h4>A developer</h4>
+      </div>
+
+      <img src={profile} alt="profile" className="home_profile-photo" />
+    </div>
+  );
+}
+
+export default Home;
