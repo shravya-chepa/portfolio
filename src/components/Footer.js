2203a8ee8b7a6585e0d37fe05965e0fa24557f45
shravya-chepa <riya.chepa@gmail.com>
2023-06-09 14:14:21 +0530

    feat: add attributes for icons and background image

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,16 @@ import "./Footer.scss";
 function Footer() {
   return (
     <div className="footer-section">
+      <div className="attributes">
+        <a href="https://www.flaticon.com/free-icons/" title="icons">
+          Icons created by Freepik, bukeicon, Pixel perfect - Flaticon
+        </a>
+        <br />
+        <a href="https://www.freepik.com/free-vector/color-seamless-space-pattern_10838001.htm#query=cartoon%20space&position=24&from_view=keyword&track=ais">
+          Background image by tartila on Freepik
+        </a>
+      </div>
+
       <p>©2023 Shravya Chepa</p>
     </div>
   );
