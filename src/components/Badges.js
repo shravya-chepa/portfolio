bbfec7e7b23477efa37afbc3592b9aeab83bdda1
shravya-chepa <riya.chepa@gmail.com>
2024-11-20 19:51:40 -0800

    change certification badge style

--- src/components/Badges.js
+++ src/components/Badges.js
@@ -34,7 +34,7 @@ const Badges = () => {
   };
 
   return (
-    <div className='badges-section'>
+    <div className='badges-section prevent-select'>
         <h1 className='star-wars'>Certifications</h1>
         <div 
       ref={containerRef}
@@ -47,7 +47,7 @@ const Badges = () => {
     
       <div className="badge red">
         <svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" width="216px" height="232px" viewBox="0 0 216 232">
-          <path fill="#2B2B2B" d="M207,0C171.827,0.001,43.875,0.004,9.003,0c-5.619-0.001-9,3.514-9,9c0,28.23-0.006,151.375,0,169 c0.005,13.875,94.499,54,107.999,54S216,191.57,216,178V9C216,3.298,212.732,0,207,0z" />
+          <path stroke="#2B2B2B" strokeWidth="4" d="M207,0C171.827,0.001,43.875,0.004,9.003,0c-5.619-0.001-9,3.514-9,9c0,28.23-0.006,151.375,0,169 c0.005,13.875,94.499,54,107.999,54S216,191.57,216,178V9C216,3.298,212.732,0,207,0z" />
         </svg>
         <p className="title">AWS</p>
         <p className="subtitle">Cloud Practitioner</p>
@@ -55,7 +55,7 @@ const Badges = () => {
       </div>
       <div className="badge green">
         <svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" width="216px" height="232px" viewBox="0 0 216 232">
-          <path fill="#2B2B2B" d="M207,0C171.827,0.001,43.875,0.004,9.003,0c-5.619-0.001-9,3.514-9,9c0,28.23-0.006,151.375,0,169 c0.005,13.875,94.499,54,107.999,54S216,191.57,216,178V9C216,3.298,212.732,0,207,0z" />
+          <path stroke="#2B2B2B" strokeWidth="4" d="M207,0C171.827,0.001,43.875,0.004,9.003,0c-5.619-0.001-9,3.514-9,9c0,28.23-0.006,151.375,0,169 c0.005,13.875,94.499,54,107.999,54S216,191.57,216,178V9C216,3.298,212.732,0,207,0z" />
         </svg>
         <p className="title">Microsoft</p>
         <p className="subtitle">Azure Fundamentals</p>
@@ -63,7 +63,7 @@ const Badges = () => {
       </div>
       <div className="badge gray">
         <svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" width="216px" height="232px" viewBox="0 0 216 232">
-          <path fill="#2B2B2B" d="M207,0C171.827,0.001,43.875,0.004,9.003,0c-5.619-0.001-9,3.514-9,9c0,28.23-0.006,151.375,0,169 c0.005,13.875,94.499,54,107.999,54S216,191.57,216,178V9C216,3.298,212.732,0,207,0z" />
+          <path stroke="#2B2B2B" strokeWidth="4" d="M207,0C171.827,0.001,43.875,0.004,9.003,0c-5.619-0.001-9,3.514-9,9c0,28.23-0.006,151.375,0,169 c0.005,13.875,94.499,54,107.999,54S216,191.57,216,178V9C216,3.298,212.732,0,207,0z" />
         </svg>
         <p className="title">Meta</p>
         <p className="subtitle">Full Stack Engineer</p>
@@ -71,7 +71,7 @@ const Badges = () => {
       </div>
       <div className="badge yellow">
         <svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" width="216px" height="232px" viewBox="0 0 216 232">
-          <path fill="#2B2B2B" d="M207,0C171.827,0.001,43.875,0.004,9.003,0c-5.619-0.001-9,3.514-9,9c0,28.23-0.006,151.375,0,169 c0.005,13.875,94.499,54,107.999,54S216,191.57,216,178V9C216,3.298,212.732,0,207,0z" />
+          <path stroke="#2B2B2B" strokeWidth="4" d="M207,0C171.827,0.001,43.875,0.004,9.003,0c-5.619-0.001-9,3.514-9,9c0,28.23-0.006,151.375,0,169 c0.005,13.875,94.499,54,107.999,54S216,191.57,216,178V9C216,3.298,212.732,0,207,0z" />
         </svg>
         <p className="title">IBM</p>
         <p className="subtitle">Data Science Program</p>
@@ -79,7 +79,7 @@ const Badges = () => {
       </div>
       <div className="badge blue">
         <svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" width="216px" height="232px" viewBox="0 0 216 232">
-          <path fill="#2B2B2B" d="M207,0C171.827,0.001,43.875,0.004,9.003,0c-5.619-0.001-9,3.514-9,9c0,28.23-0.006,151.375,0,169 c0.005,13.875,94.499,54,107.999,54S216,191.57,216,178V9C216,3.298,212.732,0,207,0z" />
+          <path stroke="#2B2B2B" strokeWidth="4" d="M207,0C171.827,0.001,43.875,0.004,9.003,0c-5.619-0.001-9,3.514-9,9c0,28.23-0.006,151.375,0,169 c0.005,13.875,94.499,54,107.999,54S216,191.57,216,178V9C216,3.298,212.732,0,207,0z" />
         </svg>
         <p className="title">University of <br />Michigan</p>
         <p className="subtitle">Python</p>
@@ -87,7 +87,7 @@ const Badges = () => {
       </div>
       <div className="badge purple">
         <svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" width="216px" height="232px" viewBox="0 0 216 232">
-          <path fill="#2B2B2B" d="M207,0C171.827,0.001,43.875,0.004,9.003,0c-5.619-0.001-9,3.514-9,9c0,28.23-0.006,151.375,0,169 c0.005,13.875,94.499,54,107.999,54S216,191.57,216,178V9C216,3.298,212.732,0,207,0z" />
+          <path stroke="#2B2B2B" strokeWidth="4" d="M207,0C171.827,0.001,43.875,0.004,9.003,0c-5.619-0.001-9,3.514-9,9c0,28.23-0.006,151.375,0,169 c0.005,13.875,94.499,54,107.999,54S216,191.57,216,178V9C216,3.298,212.732,0,207,0z" />
         </svg>
         <p className="title">Oracle<br />Academy</p>
         <p className="subtitle">Java Programming</p>
